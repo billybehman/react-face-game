2f2d3d15d64f0985f4b88307a4d1b43a98c792a6
Billy Behman <billybehman@gmail.com>
2020-07-11 09:22:35 -0400

    not going to work

---
+++ client/build/precache-manifest.f4205312628c49b74d1da24d6befeaf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0952fc35a7870f980fb4",
+    "url": "/static/css/main.505df269.chunk.css"
+  },
+  {
+    "revision": "0952fc35a7870f980fb4",
+    "url": "/static/js/main.aec63e4b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "64fb49a838e331dc17f7",
+    "url": "/static/js/2.bd2c26c7.chunk.js"
+  },
+  {
+    "revision": "b51452bf1ea8b781a109d4d17833f439",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -12,7 +12,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("./client/build"));
 }
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googleBooks";
+// mongodb:user1password1@ds125871.mlab.com:25871/heroku_0xn0jnk7
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/3001";
 
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
